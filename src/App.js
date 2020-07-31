0471c5db7df142e31cafe6e10315fc59ce5dd751
simple-sifu <kattyntommy@gmail.com>
2020-07-30 21:49:48 -0400

    add header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "Header";
 
+// switching page without refresh or reloading the page
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>   
+          <Route page="/checkout">
+            <h1>Checkout</h1>
+          </Route>
+          <Route path="/login">
+            <h1>Login Page</h1>
+          </Route>
+          <Route path="/">
+            <Header />
+            <h1>Home Page !!!</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+
   );
 }
 
---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+ 
+function Header() {
+    return (
+        <nav className="header">
+            <img className="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG7.png"></img>
+            {/* search box */}
+            {/* 3 links */}
+            {/* basket icon */}
+        </nav>
+    )
+}
+
+export default Header
