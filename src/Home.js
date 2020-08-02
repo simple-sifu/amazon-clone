1f44df7808e61094e61e51026aef0b7d3f47fb75
simple-sifu <kattyntommy@gmail.com>
2020-08-02 01:40:38 -0400

    adding product component.

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import "./Home.css";
+import Product from "./Product";
 
 function Home() {
     return (
@@ -9,6 +10,48 @@ function Home() {
             src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
             alt=""
            />
+
+
+           {/* Product id, title, price, rating,  */}
+           <div className="home__row">
+                <Product
+                    id="12321341"
+                    title="The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback"
+                    price={11.96}
+                    rating={5}
+                    image="https://upload.wikimedia.org/wikipedia/en/1/11/Lean_Startup.png"
+                />
+                <Product
+                    id="12321341"
+                    title="The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback"
+                    price={11.96}
+                    rating={5}
+                    image="https://upload.wikimedia.org/wikipedia/en/1/11/Lean_Startup.png"
+                />
+           </div>
+           <div className="home__row">
+                <Product
+                    id="12321341"
+                    title="The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback"
+                    price={11.96}
+                    rating={5}
+                    image="https://upload.wikimedia.org/wikipedia/en/1/11/Lean_Startup.png"
+                />
+                <Product
+                    id="12321341"
+                    title="The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback"
+                    price={11.96}
+                    rating={5}
+                    image="https://upload.wikimedia.org/wikipedia/en/1/11/Lean_Startup.png"
+                />
+                <Product
+                    id="12321341"
+                    title="The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback"
+                    price={11.96}
+                    rating={5}
+                    image="https://upload.wikimedia.org/wikipedia/en/1/11/Lean_Startup.png"
+                />
+           </div>
         </div>
     )
 }
