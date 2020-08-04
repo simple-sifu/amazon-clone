080b700114eca92eb67151963ed5b755d59a77c4
simple-sifu <kattyntommy@gmail.com>
2020-08-04 01:09:58 -0400

    refactor

--- src/StateProvider.js
+++ src/StateProvider.js
@@ -8,11 +8,11 @@ export const StateContext = createContext();
 
 // Build a Provider
 export const StateProvider = ({reducer, initialState, children}) => (
-    <StateContext.Provider value={useReducer(useReducer, initialState)}>
+    <StateContext.Provider value={useReducer(reducer, initialState)}>
         { children }
     </StateContext.Provider>
 )
 
 // This is how we use it inside a component
 
-export const useState = () => useContext(StateContext);
+export const useStateValue = () => useContext(StateContext);
