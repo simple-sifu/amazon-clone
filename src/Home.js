c9c1c22d3c25a5a2c689bc577db4b79ef1da349e
simple-sifu <kattyntommy@gmail.com>
2020-08-03 21:43:07 -0400

    finished product component.

--- src/Home.js
+++ src/Home.js
@@ -31,35 +31,35 @@ function Home() {
            </div>
            <div className="home__row">
                 <Product
-                    id="12321341"
-                    title="The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback"
-                    price={11.96}
+                    id="12321349"
+                    title="Skoolzy Rainbow Counting Bears with Matching Sorting Cups"
+                    price={22.96}
                     rating={5}
-                    image="https://upload.wikimedia.org/wikipedia/en/1/11/Lean_Startup.png"
+                    image="https://m.media-amazon.com/images/P/B00W8FVMNO.01-17253176625.LXXXXXXX._AC_SY480_.jpg"
                 />
                 <Product
-                    id="12321341"
-                    title="The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback"
-                    price={11.96}
+                    id="123444444"
+                    title="SaltStick Caps, Bottle of 100 Electrolyte Replacement Capsules for Rehydration"
+                    price={99.96}
                     rating={5}
-                    image="https://upload.wikimedia.org/wikipedia/en/1/11/Lean_Startup.png"
+                    image="https://images-na.ssl-images-amazon.com/images/I/41JD7kIo5VL._AC_.jpg"
                 />
                 <Product
-                    id="12321341"
-                    title="The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback"
-                    price={11.96}
+                    id="12321349"
+                    title="Qimiaobaby Diaper Bag Backpack"
+                    price={7.96}
                     rating={5}
-                    image="https://upload.wikimedia.org/wikipedia/en/1/11/Lean_Startup.png"
+                    image="https://images-na.ssl-images-amazon.com/images/I/81UQJerhwbL._SX679_.jpg"
                 />
            </div>
 
            <div className="home__row">
                 <Product
-                    id="12321341"
-                    title="The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback"
-                    price={11.96}
+                    id="12421341"
+                    title="Algorithms Illuminated: Part 1: The Basics"
+                    price={16.96}
                     rating={5}
-                    image="https://upload.wikimedia.org/wikipedia/en/1/11/Lean_Startup.png"
+                    image="https://images-na.ssl-images-amazon.com/images/I/51+cSP+PAIL._SX331_BO1,204,203,200_.jpg"
                 />
            </div>
         </div>
