053ffb033cdb13847329b40bbcead231564b652c
simple-sifu <kattyntommy@gmail.com>
2020-08-05 21:44:23 -0400

    added firebase

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase";
+
+const firebaseApp = firebase.initializeApp({
+    apiKey: "AIzaSyBfSKG8vXAyNEEA2Hxzo81D_xZlt704OPA",
+    authDomain: "clone-282ff.firebaseapp.com",
+    databaseURL: "https://clone-282ff.firebaseio.com",
+    projectId: "clone-282ff",
+    storageBucket: "clone-282ff.appspot.com",
+    messagingSenderId: "7055124161",
+    appId: "1:7055124161:web:bedc8ad8a343e47e44d640",
+    measurementId: "G-JMF490SD11"
+
+});
+
+// const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
